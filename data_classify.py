d6c8ab57fcc4bacb4b4149bf3a8800c555faa2a2
MichalTurski <turski.michal.antoni@gmail.com>
2019-05-25 11:46:43 +0200

    Changed program to work with new data: new data loading and scoring.

--- data_classify.py
+++ data_classify.py
@@ -15,26 +15,22 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # %% Import file with pandas
-#df = pd.read_csv('train.txt', sep=" ")
-df = pd.read_csv('train_prepreprocessed.csv', sep=",")
+train_data = pd.read_csv('artificial_train.data', delim_whitespace=True, header=None)
+train_labels = pd.read_csv('artificial_train.labels', delim_whitespace=True, header=None)
+valid_data = pd.read_csv('artificial_valid.data', delim_whitespace=True, header=None)
+df = pd.concat([train_data, train_labels[0]], axis=1)
 
-X_test = pd.read_csv('test_prepreprocessed.csv', sep=",")
-
-#%% Split dataset
-X = df.loc[:, df.columns != "class"]
-#X = df.loc[:, X.columns != "Var126"]
-y = df["class"]
+#%% Rename dataset
+X = train_data
+y = train_labels
 
 # %% Preprocess data
 data_transformer = data_prepare.DataTransformer()
 
-#X_train = data_transformer.fit_transform(X_train, y_train)
-#data_transformer.transform(X_test)
-
 #%% XGBoost, but run only once, cross validated
 import xgboost as xgb
 
-single_xg_switch = False
+single_xg_switch = True
 if single_xg_switch:
     max_depth = 7
     bootstrap = False
@@ -53,8 +49,8 @@ if single_xg_switch:
         X_test = X.iloc[test, :]
         y_train = y.iloc[train]
         y_test = y.iloc[test]
-        X_train = data_transformer.fit_transform(X_train, y_train)
-        X_test = data_transformer.transform(X_test)
+        # X_train = data_transformer.fit_transform(X_train, y_train)
+        # X_test = data_transformer.transform(X_test)
 
         data_dmatrix = xgb.DMatrix(data=X_train, label=y_train)
         xg_clas = xgb.XGBClassifier(max_depth=max_depth,
@@ -71,7 +67,7 @@ if single_xg_switch:
 
         # print(xg_clas.predict_proba(X_test))
         # print_classifier_scores(xg_clas, X_train, y_train, X_test, y_test)
-        results.append(top10_scorer(xg_clas, X_test, y_test))
+        results.append(balanced_scorer(xg_clas, X_test, y_test))
 
     curr_score = stat.mean(results)
     dev = stat.stdev(results)
@@ -105,8 +101,8 @@ if cv_xg_switch:
             X_test = X.iloc[test, :]
             y_train = y.iloc[train]
             y_test = y.iloc[test]
-            X_train = data_transformer.fit_transform(X_train, y_train)
-            X_test = data_transformer.transform(X_test)
+            # X_train = data_transformer.fit_transform(X_train, y_train)
+            # X_test = data_transformer.transform(X_test)
 
             data_dmatrix = xgb.DMatrix(data=X_train, label=y_train)
             xg_clas = xgb.XGBClassifier(max_depth=max_depth,
@@ -146,126 +142,6 @@ if cv_xg_switch:
       f"{best_bootstrap}, {best_min_samples_leaf}, {best_min_samples_split},"
       f"{best_n_estimators}, {best_eta}, {best_gamma}, {best_subsample},"
       f"{best_colsample_bytree}")
-
-#%% Model 2 Random Forrest
-import scipy.stats as st
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.ensemble import RandomForestClassifier
-
-rf_switch = False
-if rf_switch:
-    results = []
-    kf = KFold(n_splits=10)
-
-    best_score = 0.
-
-    for i in range(10):
-        max_depth = random.choice(np.arange(1, 15, 2))
-        bootstrap = random.choice([True, False])
-        min_samples_leaf = random.choice([1, 2, 4])
-        min_samples_split = random.choice([2, 5, 10])
-        n_estimators = random.choice(np.arange(3, 200, 10))
-
-        for train, test in kf.split(X):
-            X_train = X.iloc[train]
-            X_test = X.iloc[test, :]
-            y_train = y.iloc[train]
-            y_test = y.iloc[test]
-            X_train = data_transformer.fit_transform(X_train, y_train)
-            X_test = data_transformer.transform(X_test)
-
-            rf_clas = RandomForestClassifier(max_depth=max_depth,
-                                        bootstrap=bootstrap,
-                                        min_samples_leaf=min_samples_leaf,
-                                        min_samples_split=min_samples_split,
-                                        n_estimators=n_estimators,
-                                        n_jobs=8)
-            rf_clas.fit(X_train, y_train)
-
-            # print(xg_clas.predict_proba(X_test))
-            # print_classifier_scores(xg_clas, X_train, y_train, X_test, y_test)
-            results.append(top10_scorer(rf_clas, X_test, y_test))
-        curr_score = stat.mean(results)
-        dev = stat.stdev(results)
-        print(f"Score = {curr_score}, deviation = {dev}, params = {max_depth}, "
-              f"{bootstrap}, {min_samples_leaf}, {min_samples_split},"
-              f"{n_estimators}")
-
-        if curr_score > best_score:
-            best_score = curr_score
-            best_max_depth = max_depth
-            best_bootstrap = bootstrap
-            best_min_samples_leaf = min_samples_leaf
-            best_min_samples_split = min_samples_split
-            best_n_estimators = n_estimators
-
-    print(f"Best score = {best_score}, params = {best_max_depth}, "
-          f"{best_bootstrap}, {best_min_samples_leaf}, {best_min_samples_split},"
-          f"{best_n_estimators}")
-
-#%% Model 3: Neural network
-from network import ClassifierNetwork as cn
-
-net_swith = False
-if net_swith:
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=123)
-    X_train = data_transformer.fit_transform(X_train, y_train)
-    X_test = data_transformer.transform(X_test)
-
-    net = cn.ClassifierNetwork(X_train.shape[1])
-    net.fit(X_train, y_train)
-    # print(net.predict_proba(X_test))
-    # print(net.predict(X_test))
-    print_classifier_scores(net, X_train, y_train, X_test, y_test)
-
-
-#%% Model 4:Tree
-from sklearn import tree
-
-tree_switch = False
-if tree_switch:
-    results = []
-    kf = KFold(n_splits=10)
-
-    best_score = 0.
-
-    for i in range(20):
-        criterion = random.choice(["gini", "entropy"])
-        max_depth = random.choice(np.arange(1, 50, 2))
-        min_samples_leaf = random.choice([1, 2, 4])
-        min_samples_split = random.choice([2, 5, 10])
-
-        for train, test in kf.split(X):
-            X_train = X.iloc[train]
-            X_test = X.iloc[test, :]
-            y_train = y.iloc[train]
-            y_test = y.iloc[test]
-            X_train = data_transformer.fit_transform(X_train, y_train)
-            X_test = data_transformer.transform(X_test)
-
-            rf_clas = RandomForestClassifier(criterion = criterion,
-                                        max_depth=max_depth,
-                                        min_samples_leaf=min_samples_leaf,
-                                        min_samples_split=min_samples_split)
-            rf_clas.fit(X_train, y_train)
-
-            # print(xg_clas.predict_proba(X_test))
-            # print_classifier_scores(xg_clas, X_train, y_train, X_test, y_test)
-            results.append(top10_scorer(rf_clas, X_test, y_test))
-        curr_score = stat.mean(results)
-        dev = stat.stdev(results)
-        print(f"Score = {curr_score}, deviation = {dev}, params = {criterion}, {max_depth}, "
-              f"{min_samples_leaf}, {min_samples_split}")
-
-        if curr_score > best_score:
-            best_score = curr_score
-            best_criterion = criterion
-            best_max_depth = max_depth
-            best_min_samples_leaf = min_samples_leaf
-            best_min_samples_split = min_samples_split
-
-    print(f"Best score = {best_score}, params = {best_criterion}, "
-          f",{best_max_depth} {best_min_samples_leaf}, {best_min_samples_split},")
 
 # %% XGBoost, final classifier
     import xgboost as xgb
--- evaluation_metrics.py
+++ evaluation_metrics.py
@@ -1,35 +1,25 @@
 import pandas as pd
 import numpy as np
-from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, balanced_accuracy_score
 
 
 #%% Evaluation metrics
-def assignment_score(y_true, y_predicted):
-    pred_pairs = pd.DataFrame(np.transpose([y_true.to_numpy(), y_predicted]), columns=["y_true", "y_pred"])
-    pred_pairs = pred_pairs.sort_values(by=["y_pred"], ascending=False)
-    valuated_size = int(0.1 * pred_pairs.shape[0])
-    valuated = pred_pairs.head(n=valuated_size)
-    counts = valuated.groupby("y_true").count()
-    return counts.iloc[1].values[0] / valuated_size
-
-
-def top10_scorer(model, X, y_true):
-    y_proba = model.predict_proba(X)
-    return assignment_score(y_true, y_proba[:,1])
-
-
-def print_dataset_scores(y_true, y_predicted, y_pred_proba, set_name):
-    best_10p = assignment_score(y_true, y_pred_proba)
+def print_dataset_scores(y_true, y_predicted, set_name):
+    balanced = balanced_accuracy_score(y_true, y_predicted)
     accuracy = accuracy_score(y_true, y_predicted)
     precission = precision_score(y_true, y_predicted)
     recall = recall_score(y_true, y_predicted)
-    print(f"On {set_name} set: 10p score = {best_10p}, accuracy = {accuracy}, precission = {precission}, recall = {recall}")
+    print(f"On {set_name} set: balanced score = {balanced}, accuracy = {accuracy}, precission = {precission}, recall = {recall}")
 
 
 def print_classifier_scores(classifier, X_train, y_train, X_test, y_test):
     y_train_pred = classifier.predict(X_train)
-    y_train_proba = classifier.predict_proba(X_train)
     y_test_pred = classifier.predict(X_test)
-    y_test_proba = classifier.predict_proba(X_test)
-    print_dataset_scores(y_train, y_train_pred, y_train_proba[:, 1], "train")
-    print_dataset_scores(y_test, y_test_pred, y_test_proba[:, 1], "test")
+    print_dataset_scores(y_train, y_train_pred, "train")
+    print_dataset_scores(y_test, y_test_pred, "test")
+
+
+def balanced_scorer(classifier, X, y):
+    y_pred = classifier.predict(X)
+    balanced = balanced_accuracy_score(y, y_pred)
+    return balanced
